d92d0568b500be4b941daf2ecf3550c6e3fedae5
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-08 17:02:16 +0300

    Initial commit

---
+++ src/main/java/com/popularhollywoodmovies/hollywodsmostpopularmovies/HollywodsMostPopularMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.popularhollywoodmovies.hollywodsmostpopularmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollywodsMostPopularMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollywodsMostPopularMoviesApplication.class, args);
+	}
+
+}
